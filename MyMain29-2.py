8964422772cbed9d3528e790245ccd8430625b9b
juneyoung-goyang <pjy1365@gmail.com>
2025-03-06 11:07:18 +0900

    Add files via upload

--- MyMain29-2.py
+++ MyMain29-2.py
@@ -1,12 +1,8 @@
 import streamlit as st
-import datetime
+import pandas as pd
+import numpy as np
+import time
 
-d = st.date_input(
-    "날짜를 선택하세요",
-    datetime.date.today()
-)
-st.write('선택한 날짜:', d)
 
-st.sidebar.title("Sidebar Title")
-sidebar_option = st.sidebar.selectbox('Choose an option', ['Option 1', 'Option 2', 'Option 3'])
-st.sidebar.write('You selected:', sidebar_option)
+st.button("1")
+st.button("2")
